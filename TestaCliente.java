2a7cf6e83c6ff63e4f250df6424d8fb0ae1f1885
unknown <cinthiacaroline205@gmail.com>
2023-04-25 15:00:49 -0300

    Versão 1

---
+++ TestaCliente.java
@@ -0,0 +1,15 @@
+package poo_java;
+
+public class TestaCliente {
+
+	public static void main(String[] args) {
+		Cliente cliente1 = new Cliente ("Carla","Vestido",79,85,6);
+		Cliente cliente2 = new Cliente ("Beatriz","Tênis",90,90,0);
+		    
+		cliente1.visualizar();
+		cliente2.visualizar();
+		
+
+	}
+
+}
